eb049d857d86ac82521b1ede20ac1fead8df3ea5
SuluhAuliaArifin <suluh0376@gmail.com>
2026-05-28 03:25:46 +0700

    fix railway port debug

--- run.py
+++ run.py
@@ -2,8 +2,11 @@ import os
 import uvicorn
 
 if __name__ == "__main__":
+    port = os.getenv("PORT")
+    print("PORT VALUE =", port)
+
     uvicorn.run(
         "app.main:app",
         host="0.0.0.0",
-        port=int(os.getenv("PORT", 8000))
+        port=int(port) if port else 8000
     )
